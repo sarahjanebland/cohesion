9739b2a3a2cf49d4a7613e07518bdf3a827e9a57
Apprentice <apprentice@devbootcamp.com>
2013-04-05 11:47:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phaseboot::Application.config.secret_token = '6acf6c814e8b04c38d6905a9eee88f9f01b807ccb6f6c3da8594c5f343397a252934b39dc5294892e6266a5550aaba9259368e878d9d21f49d7a4e263fcdeba6'
