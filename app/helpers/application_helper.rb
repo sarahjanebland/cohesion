6f2d9c9fe9eace6cff1f9dc8064a60b72ec947ee
Bryan Heu <bheuclickbooq@gmail.com>
2013-04-06 23:26:13 -0700

    added links for email, iPhone, www, etc. on Profile page + helper for converting phone number to link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def phone_number_link(text)
+    sets_of_numbers = text.scan(/[0-9]+/)
+    number = "+1-#{sets_of_numbers.join('-')}"
+    link_to text, "tel:#{number}"
+  end
 end
