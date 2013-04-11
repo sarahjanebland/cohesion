d10d3b93d865415ea4b863885c54de9ea9342c02
Tyler <tyler+t@wargoats.com>
2013-04-11 16:10:37 -0700

    filled out user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,23 @@
 
 FactoryGirl.define do
   factory :user do
+    cohort
+    sequence(:github_uid) {|n| n }
+    sequence(:github_token) {|n| "token#{n}" }
+    first_name    "Joe"
+    last_name     "Doe"
+    nickname      "Jimmy D"
+    sequence(:email) {|n| "user#{n}@mail.com" }
+    phone         "123-555-1234"
+    company       "Widget Co."
+    location      "San Francisco, CA"
+    advice        "Eat, Sleep, Program, Repeat"
+    restaurant    "Pascilla"
+    photo_url     "http://dropbox.com/images/photo.jpg"
+    blog_url      "http://blog.com"
+    website_url   "website.com"
+    provider      "github"
+    facebook_url  "http://facebook.com/username"
+    twitter_url   "http://twitter.com/username"
   end
 end
