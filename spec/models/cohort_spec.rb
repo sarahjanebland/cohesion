1ba09c456e82990b57fefe297efb27bb42469e9b
Julian <juliantai90@gmail.com>
2013-04-11 12:39:14 -0700

    add have_many users

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -8,4 +8,5 @@ describe Cohort do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:start_date) }
   it { should validate_presence_of(:email) }
+  it { should have_many(:users)}
 end
