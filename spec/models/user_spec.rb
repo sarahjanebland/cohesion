d41c5d87d2e448f3bf2d27e138b18a464c007735
Julian <juliantai90@gmail.com>
2013-04-11 11:54:25 -0700

    fix guard with shoulda matchers, run 'guard' for auto test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,22 @@ require 'spec_helper'
 describe User do
 
 	it { should allow_mass_assignment_of(:first_name) }
-	
+	it { should allow_mass_assignment_of(:last_name) }
+	it { should allow_mass_assignment_of(:email) }
+	it { should allow_mass_assignment_of(:nickname) }
+	it { should allow_mass_assignment_of(:phone) }
+	it { should allow_mass_assignment_of(:job) }
+	it { should allow_mass_assignment_of(:location) }
+	it { should allow_mass_assignment_of(:advice_tweet) }
+	it { should allow_mass_assignment_of(:fav_restaurant) }
+	it { should allow_mass_assignment_of(:url) }
+	it { should allow_mass_assignment_of(:cohort_id) }
+	it { should allow_mass_assignment_of(:photo_url) }
+	it { should allow_mass_assignment_of(:uid) }
+	it { should allow_mass_assignment_of(:provider) }
+	it { should allow_mass_assignment_of(:github_token) }
+	it { should allow_mass_assignment_of(:facebook) }
+	it { should allow_mass_assignment_of(:twitter) }
   it { should belong_to(:cohort) }
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,7 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
 
+  require 'shoulda/matchers/integrations/rspec'
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
   Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
