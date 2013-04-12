7a57089bac5a7bf43b4033e867dac94b145a5c17
Tyler Kuhn <tyler@wargoats.com>
2013-04-12 12:35:23 -0700

    added validations to provider model

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,4 +1,7 @@
 class Provider < ActiveRecord::Base
   belongs_to :user
   attr_accessible :name, :secret, :token
+  
+  validates :name, :presence => true
+  validates :token, :presence => true
 end
--- spec/models/provider_spec.rb
+++ spec/models/provider_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Provider do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :user }
+  it { should validate_presence_of :name }
+  it { should validate_presence_of :token }
 end
