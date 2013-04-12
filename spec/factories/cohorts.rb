67dcdfa7a723fe60871cf07b51ba8cf7fa6d6f4b
Tyler Kuhn <tyler@wargoats.com>
2013-04-12 14:50:07 -0700

    added date sequence to cohorts

--- spec/factories/cohorts.rb
+++ spec/factories/cohorts.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :cohort do
   	sequence(:name)   {|n| "cohort#{n}" }
-  	start_date        Date.today
+  	sequence(:start_date) {|n| Date.today + n }
   	sequence(:email)  {|n| "test#{n}@email.com" }
   	photos_url        "http://dropbox.com"
   	secret_url        SecureRandom.hex(6)
