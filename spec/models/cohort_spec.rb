c3492c6fae3201ce81cd0abbbe1ef3033551cd8a
Tyler Kuhn <tyler@wargoats.com>
2013-04-12 12:45:19 -0700

    added uniqueness validation to cohort model fields

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -8,19 +8,16 @@ describe Cohort do
   it { should allow_mass_assignment_of(:start_date) }
   it { should allow_mass_assignment_of(:email) }
   it { should allow_mass_assignment_of(:photos_url) }
+  
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:start_date) }
   it { should validate_presence_of(:email) }
   it { should validate_presence_of(:secret_url) }
+
+  it { should validate_uniqueness_of(:name) }
+  it { should validate_uniqueness_of(:start_date) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:secret_url) }
+  
   it { should have_many(:users)}
-
-	it "should have secret_url 12 chars long" do
-		cohort.secret_url = "dfdsll34kfds"
-		cohort.should be_valid
-	end
-
-	it "should not have secret_urls any other length" do
-		cohort.secret_url = '2j2j2'
-		cohort.should_not be_valid
-	end
 end
