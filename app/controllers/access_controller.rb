8649cb5e475f2afeb9c84d68116ecfecce7e3699
Tyler <tyler+t@wargoats.com>
2013-04-14 15:30:26 -0700

    added a secret to the session for rejecting random users

--- app/controllers/access_controller.rb
+++ app/controllers/access_controller.rb
@@ -2,6 +2,7 @@ class AccessController < ApplicationController
   def show
   	cohort = Cohort.find_by_secret_url(params[:id])
   	if cohort
+      session[:secret] = params[:id]
   		render :pages => "index"
   	else
   		render :status => 401
