91f9e162a0bcb4db824ae14a4841967f9f8b0c49
Julian <juliantai90@gmail.com>
2013-04-15 14:25:27 -0700

    fix get requests search

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,13 @@ class PagesController < ApplicationController
   def index
     @user = User.featured || User.new
     @cohort = Cohort.all
-    search = User.search({:first_name_or_last_name_or_nickname_cont => params[:search], :cohort_name_cont => params[:view][:cohort]})
     
- 		p "^" * 300
-    p @users = search.result
+    # @users = User.all
+    if request.method == "GET"
+    	@users = User.all
+  	else
+	    search = User.search({:first_name_or_last_name_or_nickname_cont => params[:search], :cohort_name_cont => params[:view][:cohort]})
+  		@users = search.result
+  	end
   end
 end
