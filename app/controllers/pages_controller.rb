4dd6b5bb0f9b8f464f4db15aadc829870efe09d1
Julian <juliantai90@gmail.com>
2013-04-15 12:35:33 -0700

    basic search of people

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def index
     @user = User.featured || User.new
-
-    @q = User.search(params[:q])
-    @users = @q.result
+    @cohort = Cohort.all
+    search = User.search({:first_name_or_last_name_or_nickname_cont => params[:search], :cohort_name_cont => params[:view][:cohort]})
+    
+ 		p "^" * 300
+    p @users = search.result
   end
 end
