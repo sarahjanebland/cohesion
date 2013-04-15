171e0b1ed05ae5538e890615cfbcf6132c436f27
Tyler <tyler+t@wargoats.com>
2013-04-15 16:44:19 -0700

    refactored some variables

--- app/mailers/cohort_mailer.rb
+++ app/mailers/cohort_mailer.rb
@@ -2,9 +2,8 @@ class CohortMailer < ActionMailer::Base
   default from: 'hankjohnson2000@gmail.com'
 
   def signup_email(cohort)
-  	link = cohort.secret_url
-  	@url 	=	"http://bootconnect.com/access/#{link}"
-  	@cohort = cohort
+  	@url 	=	"http://boot-connect.herokuapp.com/access/#{cohort.secret_url}"
+  	@name = cohort.name
   	mail(to: cohort.email, subject: "Welcome to BootConnect!")
   end
 end
