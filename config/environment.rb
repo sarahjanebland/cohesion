b607d3ae1ab9913d2f1018f06158ed3ee5290160
Tyler <tyler+t@wargoats.com>
2013-04-15 16:42:34 -0700

    added smtp email info

--- config/environment.rb
+++ config/environment.rb
@@ -3,15 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Phaseboot::Application.initialize!
-
-# config.action_mailer.delivery_method = :smtp
-
-ActionMailer::Base.smtp_settings = {
-  :address        => "smtp.gmail.com",
-  :port           => 25,
-  :domain					=> "127.0.0.1:3000",
-  :user_name      => "hankjohnson2000@gmail.com",
-  :password       => "mvclover7829",
-  :authentication => :login
-}
-ActionMailer::Base.raise_delivery_errors = true
