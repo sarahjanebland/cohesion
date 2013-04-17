5dd5d3ff9364d2e3256fa1e0d86baa2d58078e23
Bryan Heu <teamriab@gmail.com>
2013-04-17 11:52:31 -0700

    fixed forum email notiifcation and added favicon r1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,8 @@ Phaseboot::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.action_mailer.default_url_options = { :host => "bootconnect.com" }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
