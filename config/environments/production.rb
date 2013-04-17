ea31b867555ab98e092a44c4e87c90787f041a69
Bryan Heu <teamriab@gmail.com>
2013-04-17 12:02:05 -0700

    change mail host to request host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ Phaseboot::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
-  config.action_mailer.default_url_options = { :host => "bootconnect.com" }
+  config.action_mailer.default_url_options = { :host => request.host_with_port }
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
